7f6a61faa1825f937e72bf81bbfc98358e20b938
Maxim Gantkovskiy <mgantkovski@gmail.com>
2024-12-16 19:22:10 +0300

    init commit

---
+++ tests/settings.go
@@ -0,0 +1,7 @@
+package tests
+
+var Port = 7540
+var DBFile = "../scheduler.db"
+var FullNextDate = true
+var Search = true
+var Token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOls4OSwxNDgsNzEsMjYsMTg3LDEsMTcsNDIsMjUyLDE5MywxMjksODksMjQ2LDIwNCwxMTYsMTgwLDI0NSwxNywxODUsMTUyLDYsMjE4LDg5LDE3OSwyMDIsMjQ1LDE2OSwxOTMsMTE1LDIwMiwyMDcsMTk3XX0.HBJ7UFemjWMn0LqwQcewKJXoWrPIYhkS6N3MmGN93WM`
