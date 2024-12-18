02339aa8123d2fd73ee40fb0edd271c8b18e314d
Maxim Gantkovskiy <mgantkovski@gmail.com>
2024-12-18 20:11:34 +0300

    remove redirect

--- internal/handler/router.go
+++ internal/handler/router.go
@@ -1,6 +1,8 @@
 package handler
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/paudarco/todo/internal/config"
 	"github.com/paudarco/todo/internal/service"
@@ -23,17 +25,14 @@ func NewHandler(service *service.Service, cfg *config.Config) *Handler {
 func (h *Handler) InitRouters() *gin.Engine {
 	r := gin.Default()
 
-	// Настраиваем файл-сервер.
-	r.Static("/css", "./web/css")
-	r.Static("/js", "./web/js")
-	r.StaticFile("/favicon.ico", "./web/favicon.ico")
+	// Обработчик для публичных статических файлов
+	r.NoRoute(gin.WrapH(http.FileServer(gin.Dir("./web", false))))
 
-	// Сайт работает с вышеперечисленными путями,
-	// но тесты почему-то принимают html только в виде таких двух строк.
-	r.StaticFile("/index.html", "./web/index.html")
-	r.StaticFile("/login.html", "./web/login.html")
+	r.GET("/index", func(c *gin.Context) {
+		c.File("./web/index.html")
+	})
 
-	// Обработчик для /login с редиректом на index.html, если пользователь уже авторизован
+	// Обработчик для /login с редиректом на index.html, если пользователь уже авторизован.
 	r.GET("/login", h.RedirectIfAuthenticated, func(c *gin.Context) {
 		c.File("./web/login.html")
 	})
@@ -48,9 +47,10 @@ func (h *Handler) InitRouters() *gin.Engine {
 	// Защищенные эндпоинты, требующие аутентификации.
 	protected := r.Group("/", h.AuthMiddleware)
 	{
-		protected.GET("/", func(c *gin.Context) {
-			c.File("./web/index.html")
-		})
+		// Для попадания на index.html, необходимо пройти аутентификацию.
+		// protected.GET("/", func(c *gin.Context) {
+		// 	c.File("./web/index.html")
+		// })
 
 		api := protected.Group("/api")
 		{
