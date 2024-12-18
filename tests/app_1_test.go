e88ad0ab6c2c87e4d31a4e2f0ed4b54963245c37
Maxim Gantkovskiy <mgantkovski@gmail.com>
2024-12-18 20:12:04 +0300

    remove token check

--- tests/app_1_test.go
+++ tests/app_1_test.go
@@ -26,26 +26,26 @@ func getURL(path string) string {
 }
 
 func getBody(path string) ([]byte, error) {
-	client := &http.Client{}
+	// client := &http.Client{}
 
-	req, err := http.NewRequest("GET", getURL(path), nil)
-	if err != nil {
-		return nil, err
-	}
+	// req, err := http.NewRequest("GET", getURL(path), nil)
+	// if err != nil {
+	// 	return nil, err
+	// }
 
-	req.Header.Set("Cookie", fmt.Sprintf("token=%s", Token))
+	// req.Header.Set("Cookie", fmt.Sprintf("token=%s", Token))
 
-	resp, err := client.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	// resp, err := http.Get(getURL(path))
+	// resp, err := client.Do(req)
 	// if err != nil {
 	// 	return nil, err
 	// }
 	// defer resp.Body.Close()
+
+	resp, err := http.Get(getURL(path))
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
 
 	body, err := io.ReadAll(resp.Body)
 	return body, err
